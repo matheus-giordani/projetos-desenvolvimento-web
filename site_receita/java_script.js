99bec4157b092eed656a1c4d09b9e574d03e586b
matheus giordani <mgso@ic.ufal.br>
2020-12-14 22:33:27 -0300

    usando java script

---
+++ site_receita/java_script.js
@@ -0,0 +1,17 @@
+let div1 = document.getElementById('rodape1')
+let div2 = document.getElementById('rodape2')
+
+
+div1.innerHTML = "Criado por: Matheus Giordani"
+div1.style.fontSize = "20px";
+div1.style.margin = "0px";
+div1.style.textAlign = "center";
+div1.style.background = "None";
+div1.style.padding = "0";
+div1.style.texte
+
+div2.innerHTML = "acessado em " + Date()
+div2.style.fontSize = "20px";
+div2.style.margin = "0px";
+div2.style.textAlign = "center";
+div2.style.background = "None";
